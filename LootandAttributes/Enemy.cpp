a9f48aaebf55a1562fcbc6b6d9ef8e02ccca0d64
Wilson Chang <cwswi@DESKTOP-MONLG2H>
2019-01-30 12:32:23 +0800

    Added return functions for Enemy

---
+++ LootandAttributes/Enemy.cpp
@@ -0,0 +1,48 @@
+#include "Enemy.h"
+
+
+float Enemy::return_ge()
+{
+	return ge_drop_chance;
+}
+
+float Enemy::return_gea()
+{
+	return gea_drop_chance;
+}
+
+float Enemy::return_geai()
+{
+	return geai_drop_chance;
+}
+
+float Enemy::return_iw()
+{
+	return iw_drop_chance;
+}
+
+float Enemy::return_ia()
+{
+	return ia_drop_chance;
+}
+
+float Enemy::return_ip()
+{
+	return ip_drop_chance;
+
+}
+
+float Enemy::return_id()
+{
+	return id_drop_chance;
+}
+
+int Enemy::return_xp()
+{
+	return exp;
+}
+
+int Enemy::return_gold()
+{
+	return gold;
+}
